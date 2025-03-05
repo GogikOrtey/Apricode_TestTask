64a162033a7ef2a7591faa7b80feca93eb9f34ab
Gogik <gog.ortey@yandex.ru>
2025-03-05 19:38:53 +0500

    3 Конструктор хорошо работает

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@
 // import './App.css'
 import './App.scss';
 
+console.clear();
+
 interface Task {
     id: number;		        // Уникальный номер задачи (целое число с 1)
     checked: boolean;	    // Выделена ли задача
@@ -26,7 +28,7 @@ console.log("123");
 let arrOfAllTasks_1lvl = [];
 
 // Функция, для более удобного создания задач
-function CreateTask(inp_id:number, inp_checked:boolean, inp_text_task:string, inp_parent:any, inp_children=[]): Task {
+function CreateTask(inp_id:number, inp_checked:boolean, inp_text_task:string, inp_parent:any, inp_children:any): Task {
     let newTask: Task = {
         id: inp_id,
         checked: inp_checked,
@@ -47,9 +49,60 @@ function CreateTask(inp_id:number, inp_checked:boolean, inp_text_task:string, in
 //     children: [] // Пока без подзадач
 // };
 
-let newTask = CreateTask(1, true, "123", null);
+let task1_1 = CreateTask(1, true, "task1_1", null, []);
+let task1 = CreateTask(1, true, "task1", null, [task1_1]);
 
-// Вывод объекта в консоль
-console.log("Созданная задача:", newTask);
+arrOfAllTasks_1lvl.push(task1);
+
+console.log("Все существующие задачи:");
+for(let i = 0; i < arrOfAllTasks_1lvl.length; i++) {
+    console.log("Созданная задача:", arrOfAllTasks_1lvl[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default App
